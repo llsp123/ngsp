4cf028dc23955c43df5ee3c69f6cd443739c49e2
backend-1 <backend-1@example.com>
2026-05-10 10:42:18 +0800

    引入官网的变量

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// 全局配置
+(function(){
+    window.APP_VARS = {
+        shareRedirect: 'http://43.240.156.232:8891/api.php/development/share',
+        title: '', // 自动生成
+        app_name: '南瓜视频', // 应用名
+        top_title: 'APP下载', // 子标题
+        download_title: '🚀APP下载',
+        android_url: 'https://mfsp551.lanzouo.com/iNEPw32nnxkf', // 安卓下载地址
+        android_tips: '安卓APP下载',
+        ios_url: '', // 苹果下载地址
+        ios_tips: '暂无苹果', // 苹果提示
+        mail_title: '✉️ 联络邮件', // 邮箱标题
+        mail_text: '123456@qq.com', // 邮箱地址
+        mail_tips: '希望大家能学会发送邮件获取新地址', // 邮箱提示
+        primary_address: 'nanguasp.com', // 主域名
+        alternate_address: 'nanguasp.com' // 备用域名
+    };
+    // 网页标题
+    document.title = window.APP_VARS.app_name + 'APP下载永久地址发布页-收藏我回家不迷路';
+})();
